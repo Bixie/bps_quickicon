51f68e78c9f0e4acf40d3d171ed720f50c68998f
Matthijs Alles <admin@bixie.nl>
2013-09-13 18:47:57 +0200

    Import

---
+++ bps_quickicon.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Joomla! udpate notification plugin
+ *
+ * @package		Joomla.Plugin
+ * @subpackage	Quickicon.Joomla
+ * @since		2.5
+ */
+class plgQuickiconBps_quickicon extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * @param       object  $subject The object to observe
+	 * @param       array   $config  An array that holds the plugin configuration
+	 *
+	 * @since       2.5
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	/**
+	 * Returns an icon definition for an icon which looks for extensions updates
+	 * via AJAX and displays a notification when such updates are found.
+	 *
+	 * @param  $context  The calling context
+	 *
+	 * @return array A list of icon definition associative arrays, consisting of the
+	 *				 keys link, image, text and access.
+	 *
+	 * @since       2.5
+	 */
+	public function onGetIcons($context)
+	{
+		if ($context != $this->params->get('context', 'mod_quickicon') || !JFactory::getUser()->authorise('core.manage', 'com_installer')) {
+			return;
+		}
+
+		return array(array(
+			'link' => 'index.php?option=com_bixprintshop',
+			'image' => JURI::base().'components/com_bixprintshop/assets/images/icon-48-bix-printshop.png',
+			'text' => JText::_('PLG_QUICKICON_BPS_QUICKICON_BIXPRINTSHOP'),
+			'id' => 'plg_quickicon_bps_quickicon'
+		));
+	}
+}
